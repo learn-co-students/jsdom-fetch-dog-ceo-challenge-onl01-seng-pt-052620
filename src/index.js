c77de0dd37a756d781c4e25c74cba1dd7a000840
edmundo papi <edmundopierre@yahoo.com>
2020-11-29 10:36:58 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl= "https://dog.ceo/api/breeds/image/random/4";
+fetch('imgUrl')
+.then(response =>response.json())
+.then(result =>{
+  dogImage(result.message)
+})
+
+function dogImages(dogs) {
+  console.log(dogs)
+  dogs.forEach(dog =>{
+    console.log(dog)
+    const goodBoyContainer = document.querySelector(#dog-image-contenair)
+    const goodBoy = document.createElement('img')
+    goodBoy.src = dog
+    goodBoyContainer.appendChild(goodBoy)
+  });
+}
